b4c73f154703770e49a9c710173a5eeac279e866
Infants <webmaster@infnet.id.lv>
2013-03-19 18:05:52 +0300

    Little work on site design. Added error page

---
+++ KH/app/views/kohana/error.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Error 404</title>
+</head>
+<body>
+Šāda lapa nav atrasta! Error 404.
+</body>
+</html>
--- KH/app/views/template.php
+++ KH/app/views/template.php
@@ -2,14 +2,32 @@
 <html>
 <head>
   <title>Blogs</title>
+  <meta charset='utf-8'>
   <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400&subset=latin-ext'
     rel='stylesheet'
     type='text/css'>
+  <link href='/assets/css/kube.css' rel='stylesheet' type='text/css'>
   <link href='/assets/css/style.css' rel='stylesheet' type='text/css'>
 </head>
 <body>
-<div id='content'>
-<?=$content?>
+
+<div id='header'>
+  <div id='navi'>
+    <ul>
+      <li><?=HTML::anchor('', 'Sākums')?></li>
+      <li><?=HTML::anchor('', 'Kautkas')?></li> 
+      <li><?=HTML::anchor('', 'Vēl kaut kas!')?></li>
+      <li><?=HTML::anchor('about', 'Par mani')?></li>
+   </ul>
+  </div>
 </div>
+
+<div id='content'>
+  <?=$content?>
+</div>
+
+<div id='footer'>
+</div>
+
 </body>
 </html>
