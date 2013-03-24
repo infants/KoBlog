b819726a8c91a63a78d28bc3933777a1e5270d4c
Infants <webmaster@infnet.id.lv>
2013-03-24 12:58:19 +0300

    Added userguide as submodule

--- KH/app/bootstrap.php
+++ KH/app/bootstrap.php
@@ -121,7 +121,7 @@ Kohana::modules(array(
 	// 'minion'     => MODPATH.'minion',     // CLI Tasks
 	'orm'        => MODPATH.'orm',        // Object Relationship Mapping
   // 'unittest'   => MODPATH.'unittest',   // Unit testing
-	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
+	'userguide'  => MODPATH.'userguide',  // User guide and API documentation
   'oauth2'     => MODPATH.'oauth2',
 ));
 
